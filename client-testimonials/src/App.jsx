8f522bd74a04ec24f9622e33b4e2af707857b374
Jebakumar <jebakumar.6208@gmail.com>
2025-03-26 10:10:56 +0530

    completed

---
+++ client-testimonials/src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import TestimonialCard from './components/TestimonialCard';
+import "./index.css";
+import "./App.css";
+
+const App = () => {
+  const Testimonials = [
+    {name:"Emma Watson", text:"Wonderful service, Very Impressed"},
+    {name:"Daniel Radcliffe", text:"I am in awe of the service, Really awesome"}
+  ]
+  return (
+    <div className='tm-card'>
+      <h2>Customer Testimonials</h2>
+      {Testimonials.map((testimonial, index) => (
+        <TestimonialCard key = {index} name = {testimonial.name} text = {testimonial.text}/>
+      ))}
+     </div>
+  );
+};
+
+export default App
---
+++ client-testimonials/src/components/TestimonialCard.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./TestimonialCard.css";
+
+const TestimonialCard = ({ name, text }) => {
+    <div className="tC-container">
+        <h3> Name: {name} </h3>
+        <p>  - {text} </p>
+    </div>
+}
+
+export default TestimonialCard;
